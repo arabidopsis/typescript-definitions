3b37d998a51e61bbaf7975dc7d298d77ae900728
Ian Castleden <ian.castleden@gmail.com>
2019-02-11 14:28:14 +0800

    cargo doc test fixes

--- src/lib.rs
+++ src/lib.rs
@@ -6,22 +6,22 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-//! # TypeScriptifyTrait
+//! # Generate Typescript types from Rust source code.
 //!
-//! Used with the TypeScriptify proc_macro
-//!
-//! please see documentation at [crates.io](https://crates.io/crates/typescript-definitions)
+//! Please see documentation at [crates.io](https://crates.io/crates/typescript-definitions).
 //!
 
 #![allow(unused_imports)]
+
+// we add this so `cargo doc` shows re-export.
 #[macro_use]
 pub extern crate typescript_definitions_derive;
-// re-export macros
+
+// re-export macros (note pub)
 pub use typescript_definitions_derive::*;
 use serde::ser::{Serializer};
-/// # TypeScriptifyTrait
-///
-/// Used with the `TypeScriptify` proc_macro.
+
+/// # Trait implemented by `TypeScriptify` derive macro.
 ///
 /// Please see documentation at [crates.io](https://crates.io/crates/typescript-definitions).
 ///
@@ -29,29 +29,37 @@ pub trait TypeScriptifyTrait {
     fn type_script_ify() -> String;
     // fn type_script_fields() -> Option<Vec<&'static str>>;
 }
-/// # as_byte_string
+/// # String serializer for `u8` byte buffers.
 /// 
-/// use `#[serde(serialize_with="typescript_definitions::as_byte_string")]`
+/// Use `#[serde(serialize_with="typescript_definitions::as_byte_string")]`
 /// on a `[u8]` or `Vec<u8>` object to  make the output type a `string` (instead of a `number[]`).
-/// The encoding is a simple `\xdd` format. Or provide your own serializer:
+/// The encoding is a simple `\xdd` format. 
+/// 
+/// Or provide your own serializer:
 /// `typescript-definitions` only checks the final *name* "as_byte_string" of the path.
+/// 
 /// e.g.
 /// ```
-/// # #[macro_use] extern crate typescript_definitions;
 /// # #[macro_use] extern crate serde_derive;
+/// use serde;
+/// use typescript_definitions::{TypeScriptify, TypeScriptifyTrait};
 /// 
 /// #[derive(Serialize, TypeScriptify)]
 /// struct S {
 ///     #[serde(serialize_with="typescript_definitions::as_byte_string")]
-///     image : Vec<u8>
+///     image : Vec<u8>,
+///     buffer: &'static [u8],
 /// }
+/// 
+/// println!("{}", S::type_script_ify());
 /// ```
-/// outputs `export type S = { image: string };`.
-
+/// prints `export type S = { image: string, buffer: number[] };`.
+///
 pub fn as_byte_string<'a, S>(bytes: &'a [u8], serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
-
+    // probably not possible to serialze this as a stream
+    // we have no access to the underlying io stream... :(
     let t = bytes.iter().map(|b| format!(r"\x{:02x}", b)).collect::<Vec<_>>().join("");
-    // S is FnOnce
+
     serializer.serialize_str(&t)
 
 }
